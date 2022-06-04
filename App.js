2c2325a17194ec2e7cfd44baaec62a3f92d640e2
ALI HASSAN <alihassanaha110@gmail.com>
2022-06-04 09:24:43 +0500

    Backup Site

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
 // IMPORT EXPRESS
 const express = require("express");
 const app = express();
-
-// IMPORT MONGOOSE AND SET UP DATABASE CONNECTION
-const mongoose = require("mongoose");
-mongoose
-    .connect("mongodb+srv://ALIHASSAN:ALISHBAPriNcEsS@smart-attendance-system.fa3yn.mongodb.net/smart-attendance-system-db", 
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-    .then( () => 
-    {
-        console.log("CONNECTION ESTABLISHED WITH DATABASE");
-    })
-    .catch( (error => 
-    {
-        console.log("ERROR WHILE CONNECTING DATABASE: " + error);
-    }));
-
 // IMPORT JSON FOR REQUESTED DATA INPUT AS JSON ARRAY
 app.use(express.json());
 
