4c566b920f2512bbc4bef20fe624257786b0e68c
ALI HASSAN <83202451+aliii-hassan@users.noreply.github.com>
2022-06-06 02:16:31 +0500

    Update PublicRoutes.js with password-reset route

--- Protocols/Routes/PublicRoutes.js
+++ Protocols/Routes/PublicRoutes.js
@@ -7,5 +7,6 @@ const UserController = require("../../Controllers/UserController");
 router.get("/", HomePageController.HomePage);
 router.get("/signup", UserController.SignUp);
 router.get("/signin", UserController.SignIn);
+router.get("/password-reset", UserController.PasswordReset);
 
 module.exports = router;
